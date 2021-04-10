776f8f72bacd0fb181b655bacb34e2b23eaf7131
Hien <hien03kc@gmail.com>
2021-04-10 10:20:18 +0700

    inital commit

---
+++ resources/views/homepage/homepage.blade.php
@@ -0,0 +1,17 @@
+@extends('Layout.main')
+
+@section('content')
+    <div id="content" class="no-bottom no-top">
+
+        @include('homepage.carousel');
+        @include('homepage.wedo');
+        @include('homepage.process');
+        @include('homepage.galery');
+        @include('homepage.customer');
+
+    </div>
+@endsection
+
+@section('title')
+    Homepage_Contructure
+@endsection
