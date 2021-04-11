112b7870e5e7068858f7deef015967e7df95dcbc
Hien <hien03kc@gmail.com>
2021-04-11 12:10:49 +0700

    feact: add controller project

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,24 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+    use App\Http\Controllers\project\ProjectsController;
+    use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+    /*
+    |--------------------------------------------------------------------------
+    | Web Routes
+    |--------------------------------------------------------------------------
+    |
+    | Here is where you can register web routes for your application. These
+    | routes are loaded by the RouteServiceProvider within a group which
+    | contains the "web" middleware group. Now create something great!
+    |
+    */
 
-Route::get('/', function () {
-    return view('homepage.homepage');
-})->name('homepage');
+    Route::get('/', function () {
+        return view('homepage.homepage');
+    })->name('homepage');
 
-Route::prefix('projects')->group(function(){
-    Route::get('/', 'Project.Project')->name('project');
-});
+
+    Route::prefix('project')->group(function () {
+        Route::get('/', [ProjectsController::class, 'index'])->name('project');
+    });
