148d3f073d4fb6340c8fdfd931f82d803dfd965e
duy <duynthts2006001@fpt.edu.vn>
2021-04-13 08:34:03 +0700

    update goblod in project

---
+++ resources/views/Project/GoBlog.blade.php
@@ -0,0 +1,3 @@
+<section id="call-to-action" class="call-to-action bg-color dark text-center" data-speed="5" data-type="background">
+    <a href="contact.html" class="btn btn-line-black btn-big">Get Quotation</a>
+</section>
