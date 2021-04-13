7986c5b7c5895ae86881ee945557137f2f668f2e
Hien <hien03kc@gmail.com>
2021-04-13 17:58:55 +0700

    update login form

--- resources/views/Login/FormLogin.blade.php
+++ resources/views/Login/FormLogin.blade.php
@@ -1,20 +1,21 @@
+<div class="blockForm">
 <div class="loginContent">
     <div class="title">Login</div>
 
     <form action="#">
         <div class="field">
-            <input type="text" required>
+            <input type="text" required style="padding-left: 15px" name="id">
             <label>ID</label>
         </div>
         <div class="field">
-            <input type="password" required>
+            <input type="password" required style="padding-left: 15px" name="pass">
             <label>Password</label>
         </div>
 
         <div class="content">
-            <div class="checkbox">
-                <input type="checkbox" id="remember-me">
-                <label for="remember-me">Remember me</label>
+            <div class="checkbox" style="padding-left: 0px" >
+                <input type="checkbox" id="remember-me" style="margin: 0px; padding-left: 0px">
+                <label for="remember-me" style="margin: 0">Remember me</label>
             </div>
             <div class="pass-link">
                 <a href="#">Forgot password?</a>
@@ -24,6 +25,7 @@
             <input type="submit" value="Login">
         </div>
         <div class="signup-link">
-            only for admin? <a href="#">Get Back</a></div>
+            !!!only for admin!!! <a href="{{ route('homepage') }}">Go Homepage</a></div>
     </form>
+</div>
 </div>
