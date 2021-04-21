86662d625f0e362eaf1b469944b3f5e19547ac74
Hien <hien03kc@gmail.com>
2021-04-21 11:01:45 +0700

    fix: link image team in abou us

--- database/seeders/Project/ProjectListSeeder.php
+++ database/seeders/Project/ProjectListSeeder.php
@@ -17,7 +17,6 @@ class ProjectListSeeder extends Seeder
         DB::table('project_lists')->insert([
 
             //Insert data decor căn hộ
-            //Đã insert rồi nhá
 //            ['ProjectID' => 'DCH01', 'ProjectName' => 'Căn Hộ Vinhomes Quận 1',         'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_01.jpg'],
 //            ['ProjectID' => 'DCH02', 'ProjectName' => 'Căn Hộ Masteri Quận 2',          'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_02.jpg'],
 //            ['ProjectID' => 'DCH03', 'ProjectName' => 'Căn Hộ Lumiere Resident Quận 7', 'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_03.jpg'],
@@ -25,6 +24,6 @@ class ProjectListSeeder extends Seeder
 //            ['ProjectID' => 'DCH05', 'ProjectName' => 'Căn Hộ Villa Park Quận 9',       'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_05.jpg'],
 //            ['ProjectID' => 'DCH06', 'ProjectName' => 'Căn Hộ King Center Quận 10',     'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_06.jpg'],
 //            ['ProjectID' => 'DCH07', 'ProjectName' => 'Căn Hộ Begenco Quận 11',         'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_07.jpg'],
-        ]);
+//        ]);
     }
 }
