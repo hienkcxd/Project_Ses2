05a5951394dac1e5443699ed8bfc3100e2fca53d
Vuong <a0935481428@gmail.com>
2021-04-22 15:26:13 +0700

    vuong up

--- database/seeders/Project/ProjectDetailSeeder.php
+++ database/seeders/Project/ProjectDetailSeeder.php
@@ -107,6 +107,69 @@ class ProjectDetailSeeder extends Seeder
 //                'contentTop'  => 'Đây là nội dung bên trên',
 //                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
 
+//
+//            [   'ProDetailID' => 'BT01', 'ProjectID'       => 'BT01',
+//                'DateFinish'  => 'February 2021', 'Location' => 'Bá Đa Lộc', 'Price' => '19 tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_01.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_01.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//
+//            [   'ProDetailID' => 'BT02', 'ProjectID'       => 'BT02',
+//                'DateFinish'  => 'Janvary 2021', 'Location' => 'Đường Lý Thường Kiệt', 'Price' => '27 tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_02.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bop_02.jpg.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//            [   'ProDetailID' => 'BT03', 'ProjectID'       => 'BT03',
+//                'DateFinish'  => 'March 2021', 'Location' => 'Đường Hai Bà Trưng', 'Price' => '30tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_03.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_03.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//            [   'ProDetailID' => 'BT04', 'ProjectID'       => 'BT04',
+//                'DateFinish'  => 'April 2021', 'Location' => 'Trần Hưng Đạo', 'Price' => '35tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_04.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_04.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//            [   'ProDetailID' => 'BT05', 'ProjectID'       => 'BT05',
+//                'DateFinish'  => 'April 2021', 'Location' => 'Trần Phú', 'Price' => '40tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_05.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_05.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//            [   'ProDetailID' => 'BT06', 'ProjectID'       => 'BT06',
+//                'DateFinish'  => 'May 2021', 'Location' => 'Trần Phú', 'Price' => '45tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_06.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_06.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//            [   'ProDetailID' => 'BT07', 'ProjectID'       => 'BT07',
+//                'DateFinish'  => 'June 2021', 'Location' => 'Đinh Tiên Hoàng', 'Price' => '50tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_07.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_07.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+//
+//
+//            [   'ProDetailID' => 'BT08', 'ProjectID'       => 'BT08',
+//                'DateFinish'  => 'June 2021', 'Location' => 'Phước Long A', 'Price' => '55tỷ VND', 'Client' => 'Mr.Vương', 'tagName' => 'Biệt Thự',
+//                'imageTop'    => 'images\Project\bietthu\Detail\Detail_BT_top_08.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_08.jpg',
+//                'contentTop'  => 'Đây là nội dung bên trên',
+//                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         ]);
