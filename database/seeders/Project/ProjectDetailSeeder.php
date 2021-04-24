9c74d903323c7f8c92a1886d77366b7bd1dd6782
ISAO1999626 <isao1999626@gmail.com>
2021-04-24 10:07:55 +0700

    merge main into my branches

--- database/seeders/Project/ProjectDetailSeeder.php
+++ database/seeders/Project/ProjectDetailSeeder.php
@@ -21,7 +21,7 @@
 //                'imageTop'    => 'images\Project\canho\Detail\DCH_detail_top_01.jpg', 'imageBot' => 'images\Project\canho\Detail\DCH_detail_bot_01.jpg',
 //                'contentTop'  => 'Đây là nội dung bên trên',
 //                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
-//
+////
 //            [   'ProDetailID' => 'DCH02', 'ProjectID'       => 'DCH02',
 //                'DateFinish'  => 'March 2020', 'Location' => 'Mai Chí Thọ, Quận 2', 'Price' => '600 triệu VND', 'Client' => 'Mr.Duy', 'tagName' => 'Decor Căn Hộ',
 //                'imageTop'    => 'images\Project\canho\Detail\DCH_detail_top_02.jpg', 'imageBot' => 'images\Project\canho\Detail\DCH_detail_bot_02.jpg',
@@ -106,9 +106,9 @@
 //                'imageTop'    => 'images\Project\nhapho\Detail\NP_detail_top_08.jpg', 'imageBot' => 'images\Project\nhapho\Detail\NP_detail_bot_08.jpg',
 //                'contentTop'  => 'Đây là nội dung bên trên',
 //                'contentBot'  => 'Đây là nội dung giới thiệu bên dưới' ],
-
-
-//                //Insert data biệt thự
+//
+//
+////                //Insert data biệt thự
 //                ['ProDetailID' => 'BT01', 'ProjectID' => 'BT01',
 //                    'DateFinish' => 'February 2021', 'Location' => 'Trần Hưng Đạo, Quận 1', 'Price' => '19 tỷ VND', 'Client' => 'Ms.Hoa', 'tagName' => 'Biệt Thự',
 //                    'imageTop' => 'images\Project\bietthu\Detail\Detail_BT_top_01.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_01.jpg',
@@ -158,9 +158,9 @@
 //                    'imageTop' => 'images\Project\bietthu\Detail\Detail_BT_top_08.jpg', 'imageBot' => 'images\Project\bietthu\Detail\Detail_BT_bot_08.jpg',
 //                    'contentTop' => 'Đây là nội dung bên trên',
 //                    'contentBot' => 'Đây là nội dung giới thiệu bên dưới'],
-//
-//
-////Insert data nhà tân cổ
+////
+////
+//////Insert data nhà tân cổ
 //                ['ProDetailID' => 'NTC01', 'ProjectID' => 'NTC01',
 //                    'DateFinish' => 'June 2021', 'Location' => 'Cách Mạng Tháng 8, Quận 1', 'Price' => '27 tỷ VND', 'Client' => 'Ms.Thắm', 'tagName' => 'Nhà Tân Cổ',
 //                    'imageTop' => 'images\Project\tanco\Detail\Detail_NTC_top_01.jpg', 'imageBot' => 'images\Project\tanco\Detail\Detail_NTC_bot_01.jpg',
--- database/seeders/Project/ProjectListSeeder.php
+++ database/seeders/Project/ProjectListSeeder.php
@@ -26,8 +26,8 @@ class ProjectListSeeder extends Seeder
 //            ['ProjectID' => 'NP06', 'ProjectName' => 'Nhà Phố Quận 6',         'TagName' => 'NP', 'images' => 'images\Project\nhapho\NP_bg_06.jpg'],
 //            ['ProjectID' => 'NP07', 'ProjectName' => 'Nhà Phố Quận 7',         'TagName' => 'NP', 'images' => 'images\Project\nhapho\NP_bg_07.jpg'],
 //            ['ProjectID' => 'NP08', 'ProjectName' => 'Nhà Phố Quận 8',         'TagName' => 'NP', 'images' => 'images\Project\nhapho\NP_bg_08.jpg'],
-
-//            //Insert data decor căn hộ
+//
+////            //Insert data decor căn hộ
 //            ['ProjectID' => 'DCH01', 'ProjectName' => 'Căn Hộ Vinhomes Quận 1',         'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_01.jpg'],
 //           ['ProjectID' => 'DCH02', 'ProjectName' => 'Căn Hộ Masteri Quận 2',          'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_02.jpg'],
 //            ['ProjectID' => 'DCH03', 'ProjectName' => 'Căn Hộ Lumiere Resident Quận 7', 'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_03.jpg'],
@@ -35,8 +35,8 @@ class ProjectListSeeder extends Seeder
 //           ['ProjectID' => 'DCH05', 'ProjectName' => 'Căn Hộ Villa Park Quận 9',       'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_05.jpg'],
 //            ['ProjectID' => 'DCH06', 'ProjectName' => 'Căn Hộ King Center Quận 10',     'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_06.jpg'],
 //            ['ProjectID' => 'DCH07', 'ProjectName' => 'Căn Hộ Begenco Quận 11',         'TagName' => 'DCH', 'images' => 'images\Project\canho\DCH_bg_07.jpg'],
-//
-//        //insert data biet thu
+////
+////        //insert data biet thu
 //            ['ProjectID' => 'BT01', 'ProjectName' => 'Biệt Thự Hiện Đại Quận 1',  'TagName' => 'BT', 'images' => 'images\Project\bietthu\BT_bg_01.jpg'],
 //            ['ProjectID' => 'BT02', 'ProjectName' => 'Biệt Thự Hiện Đại Quận 2',  'TagName' => 'BT', 'images' => 'images\Project\bietthu\BT_bg_02.jpg'],
 //            ['ProjectID' => 'BT03', 'ProjectName' => 'Biệt Thự Hiện Đại Quận 3',  'TagName' => 'BT', 'images' => 'images\Project\bietthu\BT_bg_03.jpg'],
@@ -45,8 +45,8 @@ class ProjectListSeeder extends Seeder
 //            ['ProjectID' => 'BT06', 'ProjectName' => 'Biệt Thự Hiện Đại Quận 6 ', 'TagName' => 'BT', 'images' => 'images\Project\bietthu\BT_bg_06.jpg'],
 //            ['ProjectID' => 'BT07', 'ProjectName' => 'Biệt Thự Hiện Đại Quận 7 ', 'TagName' => 'BT', 'images' => 'images\Project\bietthu\BT_bg_07.jpg'],
 //            ['ProjectID' => 'BT08', 'ProjectName' => 'Biệt Thự Hiện Đại Quận 8',  'TagName' => 'BT', 'images' => 'images\Project\bietthu\BT_bg_08.jpg'],
-//
-//        //insert data nhà tân cổ
+////
+////        //insert data nhà tân cổ
 //            ['ProjectID' => 'NTC01', 'ProjectName' => 'Nhà Tân Cổ Quận 1 ',   'TagName' => 'NTC', 'images' => 'images\Project\tanco\NTC_bg_01.jpg'],
 //            ['ProjectID' => 'NTC02', 'ProjectName' => 'Nhà Tân Cổ Quận 2 ',   'TagName' => 'NTC', 'images' => 'images\Project\tanco\NTC_bg_02.jpg'],
 //            ['ProjectID' => 'NTC03', 'ProjectName' => 'Nhà Tân Cổ Quận 3',    'TagName' => 'NTC', 'images' => 'images\Project\tanco\NTC_bg_03.jpg'],
