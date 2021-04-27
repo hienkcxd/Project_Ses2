3dbd69649efeef5f637ff7655565ee5560ee9874
Hien <hien03kc@gmail.com>
2021-04-27 09:51:37 +0700

    update project full: =>đã chứa nội dung của Huân, Duy.

--- database/seeders/AboutUs/EmployeeDetail.php
+++ database/seeders/AboutUs/EmployeeDetail.php
@@ -14,7 +14,7 @@ class EmployeeDetail extends Seeder
      */
     public function run()
     {     DB::table('employee_details')->insert([
-       ['ID'=>'1' , 'EmployeeID'=>'A01' , 'empName'=>'a' , 'position'=>'a' , 'describe'=>'aaa' , 'email'=>'abc@gmail.com' , 'phone'=>'123241313' , 'facebook'=>'' , 'zalo'=>'' , 'images'=>'images\AboutUs\team_pic_1.jpg' ],
+        ['ID'=>'1' , 'EmployeeID'=>'A01' , 'empName'=>'a' , 'position'=>'a' , 'describe'=>'aaa' , 'email'=>'abc@gmail.com' , 'phone'=>'123241313' , 'facebook'=>'' , 'zalo'=>'' , 'images'=>'images\AboutUs\team_pic_1.jpg' ],
         ['ID'=>'2' , 'EmployeeID'=>'A02' , 'empName'=>'b' , 'position'=>'b' , 'describe'=>'aaa' , 'email'=>'abc@gmail.com' , 'phone'=>'123241313' , 'facebook'=>'' , 'zalo'=>'' , 'images'=>'images\AboutUs\team_pic_2.jpg' ],
         ['ID'=>'3' , 'EmployeeID'=>'A03' , 'empName'=>'c' , 'position'=>'c' , 'describe'=>'aaa' , 'email'=>'abc@gmail.com' , 'phone'=>'123241313' , 'facebook'=>'' , 'zalo'=>'' , 'images'=>'images\AboutUs\team_pic_3.jpg' ],
         ['ID'=>'4' , 'EmployeeID'=>'A04' , 'empName'=>'d' , 'position'=>'d' , 'describe'=>'aaa' , 'email'=>'abc@gmail.com' , 'phone'=>'123241313' , 'facebook'=>'' , 'zalo'=>'' , 'images'=>'images\AboutUs\team_pic_4.jpg' ],
