b196de1552f817b34cf35b24cc9472847990b52e
Hien <hien03kc@gmail.com>
2021-05-03 10:41:13 +0700

    quận bình tân

--- database/seeders/Market/MarketList.php
+++ database/seeders/Market/MarketList.php
@@ -786,6 +786,13 @@ class MarketList extends Seeder
                 'Thang_5'=>, 'Thang_6'=>, 'Thang_7'=>, 'Thang_8'=>,
                 'Thang_9'=>, 'Thang_10'=>, 'Thang_11'=>, 'Thang_12'=>,
             ],
+
+            //Gia đất quận Bình Tân - chưa edit.
+            ['MarketID'=> 152, 'DistrictName'=> 'Quận Bình Tân', 'WardName'=>'An Lạc', 'Year'=>'2020',
+                'Thang_1'=>random_int(), 'Thang_2'=>, 'Thang_3'=>, 'Thang_4'=>,
+                'Thang_5'=>, 'Thang_6'=>, 'Thang_7'=>, 'Thang_8'=>,
+                'Thang_9'=>, 'Thang_10'=>, 'Thang_11'=>, 'Thang_12'=>,
+            ],
         ]);
     }
 }
